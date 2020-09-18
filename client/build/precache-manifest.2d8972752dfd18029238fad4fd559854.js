8c8e6ec3c2f36506b309c7c25144ead153ecf776
Robert Santos <rasbms81608@gmail.com>
2020-09-18 14:48:57 -0500

    inital commit

---
+++ client/build/precache-manifest.2d8972752dfd18029238fad4fd559854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0350cc5be0b05b470509d353c712dec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d57adc8d9811aeaf4d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6bb5ce93a5037b9a4ff3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2d57adc8d9811aeaf4d",
+    "url": "/static/js/2.391e8620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.391e8620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb5ce93a5037b9a4ff3",
+    "url": "/static/js/main.82fcf14e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
